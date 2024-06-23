51a9d9acbcee28c659a196c841b42b84eda59f23
nijesmik <bkybb@duck.com>
2024-06-24 04:09:56 +0900

    refactor(week04): n^2 배열 자르기

--- nijesmik/week04/n^2_배열_자르기.js
+++ nijesmik/week04/n^2_배열_자르기.js
@@ -1,31 +1,10 @@
 function solution(n, left, right) {
-  const startC = left % n;
-  const startR = (left - startC) / n;
-
-  const endC = right % n;
-  const endR = (right - endC) / n;
-
   const ans = [];
 
-  if (startR == endR) {
-    for (let j = startC; j <= endC; j++) {
-      ans.push(Math.max(startR, j) + 1);
-    }
-    return ans;
-  }
-
-  for (let j = startC; j < n; j++) {
-    ans.push(Math.max(startR, j) + 1);
-  }
-
-  for (let i = startR + 1; i < endR; i++) {
-    for (let j = 0; j < n; j++) {
-      ans.push(Math.max(i, j) + 1);
-    }
-  }
-
-  for (let j = 0; j <= endC; j++) {
-    ans.push(Math.max(endR, j) + 1);
+  for (let i = left; i <= right; i++) {
+    const col = i % n;
+    const row = (i - col) / n;
+    ans.push(Math.max(row, col) + 1);
   }
 
   return ans;
