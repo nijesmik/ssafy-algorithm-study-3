3b3f8a7f560dac280143914d03efaca815f52d53
damdam6 <dambi626626@gmail.com>
2024-07-11 08:30:10 +0900

    fix(week05): 오류 확인 목적 코드 업로드

--- damdam6/week05/아방가르드 타일링.java
+++ damdam6/week05/아방가르드 타일링.java
@@ -1,67 +1,53 @@
 import java.util.*;
 
 class Solution {
-    long MOD = 1_000_000_007;
-    
     public int solution(int n) {
-        int answer = 0;
         
-        long[] cnt = new long[Math.max(n + 1, 7)];
+        int MOD = 1_000_000_007;
         
+        int[] dp = new int[n+1];
         
-        cnt[1] = getRemainerFromInt(1);
-        cnt[2] = getRemainerFromInt(2);
-        cnt[3] = getRemainerFromInt(5);
-        cnt[4] = getRemainerFromInt(2);
-        cnt[5] = getRemainerFromInt(2);
-        cnt[6] = getRemainerFromInt(4);
+        dp[1] = 1;
+        if(n == 1)return dp[1];
+        dp[2] = 3;
+        if(n == 2)return dp[2];
+        dp[3] = 10;
+        if(n == 3)return dp[3];
+        dp[4] = 23;
+        if(n==4)return dp[4];
+        dp[5] = 62;
+        if(n==5)return dp[5];
+        dp[6] = 170;
         
-        for(int i=1;i<=n;i++){
-            if(i+1 <=n){
-                 cnt[i+1] = getRemainer(cnt[i+1]+cnt[i]);
-            }
-           
-            if(i+2 <= n){
-            cnt[i+2] = getRemainer(cnt[i+2]+cnt[i] * 2);
-            }
-            
-            if(i+3 <= n){
-            cnt[i+3] = getRemainer(cnt[i+3] + cnt[i] * 5);
-            }
-            
-            int k = 4;
-            
-            while(i+k <= n){
-                
-                
-                cnt[i+k] = getRemainer(cnt[i+k] + cnt[i]*2);
-                
-                if(i+k+1 <= n){
+        for(int i=7;i<=n;i++){
 
-                    cnt[i+k+1] = getRemainer(cnt[i+k+1] + cnt[i]*2);
-                }
-                if(i+k+2 <= n){
-                    cnt[i+k+2] = getRemainer(cnt[i+k+2] + cnt[i] * 4);
-                }
-                
-                k+= 3;
+            
+            dp[i] =(dp[i-1]%MOD + dp[i-2]*2%MOD + dp[i-3]*5%MOD)%MOD;
+            
+            // System.out.println(dp[i]);
+            
+            int k = i - 4;
+            while (k > 0) {
+                dp[i] = (dp[i] + dp[k] * 2 % MOD) % MOD;
+                k -= 3;
+            }
+
+            k = i - 5;
+            while (k > 0) {
+                dp[i] = (dp[i] + dp[k] * 2 % MOD) % MOD;
+                k -= 3;
+            }
+
+            k = i - 6;
+            while (k > 0) {
+                dp[i] = (dp[i] + dp[k] * 4 % MOD) % MOD;
+                k -= 3;
             }
             
-            System.out.println(Arrays.toString(cnt));
+         System.out.println(Arrays.toString(dp));
         }
-        
-        answer = (int) cnt[n];
-        
+
+        int answer = dp[n];
         return answer;
     }
-    
-    long getRemainerFromInt(int num){
-        return num%MOD;
-    }
-    
-    long getRemainer(long num){
-        return num%MOD;
-    }
-    
-    
 }
