5febbedb64b28ebdf9d00218a0296844a1bb2010
damdam6 <dambi626626@gmail.com>
2024-07-22 11:49:29 +0900

    fix(week08): 110 옮기기 - 오답 수정

--- damdam6/week08/110 옮기기.java
+++ damdam6/week08/110 옮기기.java
@@ -1,112 +1,76 @@
 import java.util.*;
 
+
 class Solution {
     public String[] solution(String[] s) {
-        int length = s.length;
-        String[] answer = new String[length];
+        String[] answer = new String[s.length];
 
-        int countX;
-        for(int t = 0; t<length; t++){
-            countX = 0;
-        // 뽑을 때는? -> 다 뽑기
-            char[] cArr = s[t].toCharArray();
-            
-            int i = 0;
-            int record = 0;
-             while(i < cArr.length - 2){
-                record = i;
-                int a = i;
-                 
-                if(a >= cArr.length)break;
-                while(cArr[a] == '.'){
-                    
-                    a++;
-                };
-                 
-                 // 시작점 바꾸기
-                 if(cArr[a] == '0'){
-                     // 0이므로 어차피 시작 안됨
-                     i = a+1;
-                     continue;
-                 }
-                int b = a+1;
-                 if(b >= cArr.length)break;
-                while(cArr[b] == '.'){
-                    b++;
-                }
-                 if(cArr[b] == '0'){
-                     i = b+1;
-                     continue;
-                 }
-                int c = b+1;
-                 if( c >= cArr.length)break;
-                 while(cArr[c] == '.'){
-                     c++;
-                 }
-                 if(cArr[c] == '1'){
-                     i = b;
-                     continue;
-                 }
-                 
-                 countX ++;
-                 cArr[a] = '.';
-                 cArr[b] = '.';
-                cArr[c] = '.';
-                 i = ;
-            }
-            
-            System.out.println(Arrays.toString(cArr));
-            
-            // for(int i=0; i<cArr.length-2;i++){
-            //     if(cArr[i] == '1' 
-            //       && cArr[i+1] == '1' 
-            //       && cArr[i+2] == '0' ){
-            //         countX++;
-            //         cArr[i] = '.';
-            //         cArr[i+1] = '.';
-            //         cArr[i+2] = '.';
-            //     }
-            // }
-            
-        // 0을 앞 쪽으로 밀어낼 수 있도록 뒤에 달기
-          
-            int idx = cArr.length-1;
-            String newS = "";
-            while(idx >= 0 ){
-                if(cArr[idx] == '.'){
-                    idx--;
-                }else if(cArr[idx] == '0'){
-                    idx--;
-                    // 더할 110이 없으면
-                    if(countX <= 0){ 
-                        newS = "0"+newS;
+        for(int test=0; test < s.length; test++){
+
+            // System.out.println("test"+test);
+
+            String str = s[test];
+
+            Stack<Character> stk = new Stack<>();
+
+
+            int idx = 0;
+            int countX = 0;
+            while(idx >= 0 && idx < str.length()){
+                stk.push(str.charAt(idx++));
+
+
+
+                if(stk.size()>=3){
+                    char a = stk.pop();
+                    char b = stk.pop();
+                    char c = stk.pop();
+
+                    if(a=='0' && b=='1' && c == '1'){
+                        countX++;
                         continue;
                     }
-                    // 있을 경우
-                    newS = "0110"+newS;
-                    countX--;
-                }else{
-                   newS = "1"+newS;
-                    idx--;
-                }
-                
-                if(t==2){
-                    System.out.println(newS);
+
+                    stk.push(c);
+                    stk.push(b);
+                    stk.push(a);
+
                 };
+
             }
-            
-            while(countX > 0){
-                newS = "110"+newS;
-                countX--;
+            // System.out.println(stk.toString());
+
+            StringBuilder sb = new StringBuilder();
+
+            int size = stk.size();
+            for(int i=0; i< size;i++){
+                char t = stk.pop();
+                if(t == '0'){
+                    stk.push(t);
+                    break;
+                }
+                sb.insert(0,t);
             }
-            
-            
-            answer[t] = newS;
-        
-            
+
+            // System.out.println("stk: "+stk.toString());
+            // System.out.println("sb: "+sb.toString());
+            for(int i=0; i<countX;i++){
+                sb.insert(0,"110");
+            }
+
+            while(!stk.isEmpty()){
+                sb.insert(0, stk.pop());
+            }
+
+
+
+
+
+
+            answer[test] = sb.toString();
         }
-        
-        
+
+
         return answer;
     }
 }
