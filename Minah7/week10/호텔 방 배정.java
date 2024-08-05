3d9be6748e5b8a9acdc43aa1b4125153c9ad8c9f
Minah7 <minahpark42@gmail.com>
2024-08-05 15:21:40 +0900

    fix(week10): 호텔 방 배정

--- Minah7/week10/호텔 방 배정.java
+++ Minah7/week10/호텔 방 배정.java
@@ -1,18 +1,22 @@
-// 미완성
+// 정확성 성공, 효율성 실패.
+
+import java.util.*;
 
 class Solution {
     public long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-        boolean[] check = new boolean[room_number.length];
-        for(long i = 0; i < room_number.length; i++) {
-            if(!check[room_number[i]]) {
-                check[room_number[i]] = true;
-                answer[i] = room_number[i];
+        Set<Long> set = new HashSet<>();
+        for(int i = 0; i < room_number.length; i++) {
+            long room = room_number[i];
+            if(!set.contains(room)) {
+                set.add(room);
+                answer[i] = room;
             } else {
-                for(long start = room_number[i] + 1; start < room_number.length; start++) {
-                    if(!check[start]) {
-                        check[start] = true;
+                for(long start = room + 1; start <= k; start++) {
+                    if(!set.contains(start)) {
+                        set.add(start);
                         answer[i] = start;
+                        break;
                     }
                 }
             }
